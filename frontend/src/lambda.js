708f32ec5aa4f3d29a525ce8baf0f33a063fd545
Edward Bridges <github@eqbridges.com>
2019-08-12 07:00:50 -0400

    configure interaction with upload signing endpoint. it requires: csrf token, multipart-form content type, and receives the upload url in the location header

--- frontend/src/lambda.js
+++ frontend/src/lambda.js
@@ -4,13 +4,18 @@ import config from './config'
 export default {
   getSignedURL (file) {
     let endpoint = config.AWS_LAMBDA_GETSIGNEDURL_ENDPOINT
-    let payload = {
-      filePath: file.name,
-      contentType: file.type
+    let bodyFormData = new FormData()
+    bodyFormData.set('mime_type', file.type)
+
+    let csrfToken = document.cookie.split('=')[1]
+    let headers = {
+      'X-CSRFToken': csrfToken,
+      'Content-Type': 'multipart/form-data'
     }
-    return axios.post(endpoint, payload)
+
+    return axios.post(endpoint, bodyFormData, {headers: headers})
       .then((res) => {
-        return Promise.resolve(res.data.url || '/')
+        return Promise.resolve(res.headers['Location'] || '/')
       })
       .catch((err) => {
         console.error(err)
